a1e0109244edebf7e267f5f59e10174ba07c2e75
darkelixir <thealchemist20@protonmail.com>
2023-05-03 14:44:06 +0530

    Removing some stuff

--- src/main/java/routes/Routes.java
+++ src/main/java/routes/Routes.java
@@ -13,16 +13,5 @@ public class Routes {
 
     public static String createUser_url = baseurl+"/api/users";
     
-    
-    
-    // Routes for gorest.co.in
-    public static String gorestbaseurl = "https://gorest.co.in";
-    
-    public static String post_url = gorestbaseurl + "/public/v2/users/";
-    
-    public static String get_url = gorestbaseurl+ "/public/v2/users/{id}";
-    
-    public static String update_url = gorestbaseurl+ "/public/v2/users/{id}";
-    
-    public static String delete_url = gorestbaseurl+ "/public/v2/users/{id}";
+   
 }
